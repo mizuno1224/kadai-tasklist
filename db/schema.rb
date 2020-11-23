b947130b5d85c06c2cd89151386ae80c156477e1
mizu.masa <m.mizuno1224@gmail.com>
2020-11-23 11:07:18 +0000

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_102019) do
+ActiveRecord::Schema.define(version: 2020_11_23_103542) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
